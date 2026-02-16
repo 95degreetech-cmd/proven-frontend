7759be79f564fe7cb04dafbd97e81a7d68fec412
95degreetech-cmd <95degreetech@gmail.com>
2026-02-16 11:50:14 +0300

    Create Hero component file

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <h1>Welcome to Our Website</h1>
+            <p>This is the hero component.</p>
+        </div>
+    );
+};
+
+export default Hero;
