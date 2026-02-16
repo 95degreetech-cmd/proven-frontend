85393c957c795e146575b8e84130925797446c78
95degreetech-cmd <95degreetech@gmail.com>
2026-02-16 11:53:42 +0300

    Create HowItWorks component file

---
+++ src/components/HowItWorks.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HowItWorks = () => {
+    return (
+        <div>
+            <h2>How It Works</h2>
+            <p>This component explains how our application works.</p>
+        </div>
+    );
+};
+
+export default HowItWorks;
