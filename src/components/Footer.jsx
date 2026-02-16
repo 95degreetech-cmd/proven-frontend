28bc2a9426dbbad2220db6f4c8960faa69ccf3d6
95degreetech-cmd <95degreetech@gmail.com>
2026-02-16 11:53:28 +0300

    Create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} 95degreetech-cmd. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
