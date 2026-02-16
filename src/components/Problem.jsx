a753cfe992f647ab9da4e747328564ef76c7bf96
95degreetech-cmd <95degreetech@gmail.com>
2026-02-16 11:52:49 +0300

    Create Problem component

---
+++ src/components/Problem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Problem = () => {
+    return (
+        <div>
+            <h1>Problem Component</h1>
+            <p>This is the Problem component!</p>
+        </div>
+    );
+};
+
+export default Problem;
